644b98e86f17f0cd7d36242524b4db5366db9b47
Jittat Fakcharoenphol <jittat@gmail.com>
2025-01-16 04:37:46 +0700

    started upgrading to 7.0

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_12_102327) do
+ActiveRecord::Schema[6.1].define(version: 2018_06_12_102327) do
 
   create_table "announcements", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "author"
